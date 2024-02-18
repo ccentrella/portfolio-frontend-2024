2c5cfe7bcbee2f685e81e203f24b2d51561c98ac
Christopher Centrella <ccentrella.j@gmail.com>
2024-02-18 08:50:38 -0600

    In progress: work on homepage

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function Card({title, children}: {title?: string, children: any}) {
+function Card({title, subtitle, image, children}: {
+    title?: string,
+    subtitle?: string,
+    image?: string,
+    children: any
+}) {
+    const titleColor = subtitle ? 'text-cyan-100' : 'text-cyan-300';
     return (
-        <div className={'p-7 max-w-96 rounded-2xl bg-cyan-900 shadow-xl shadow-cyan-100 hover:bg-cyan-800 hover:shadow-2xl hover:shadow-cyan-300'}>
-            <p className={'text-xl text-cyan-300 font-extrabold pb-1'}>{title}</p>
-            <p className={'text-blue-100'}>
+        <div
+            className={'p-7 max-w-96 rounded-2xl bg-cyan-900 shadow-xl shadow-cyan-100 hover:bg-cyan-800 hover:shadow-2xl hover:shadow-cyan-300'}>
+            <div className={'flex'}>
+                {image && <img src={image} alt={'card image'} className={'w-12 h-12 mr-5'}/>}
+                <div>
+                    {title && <p className={`text-xl ${titleColor} font-extrabold`}>{title}</p>}
+                    {subtitle && <p className={'text-lg text-cyan-300 font-extrabold'}>{subtitle}</p>}
+                </div>
+            </div>
+            <p className={'text-blue-100 pt-2'}>
                 {children}
             </p>
         </div>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,28 +10,47 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
     return (
-        <div className={'p-20'}>
-            <div className={''}>
-                <p className={'text-6xl text-cyan-900 font-extrabold pb-2.5 pt-[20vh]'}>Chris Centrella</p>
-                <p className={'text-6xl text-cyan-500 font-extrabold pb-10'}>Frontend Engineer</p>
+        <div>
+            <div className={'my-10 flex flex-wrap justify-center'}>
+                <div className={'m-5 px-10 py-40 w-[90%] md:w-[50%] bg-cyan-100 rounded-3xl'}>
+                    <p className={'pb-7 text-8xl lg:text-9xl text-cyan-500 font-extrabold'}>Hey,</p>
+                    <p className={'text-6xl lg:text-7xl ml-1 text-cyan-700 font-extrabold pb-10 transition-opacity duration-500'}>I'm
+                        Chris</p>
+                </div>
+                <div className={'m-5 px-10 py-40 w-[90%] md:w-[30%] bg-cyan-100 rounded-3xl'}>
+                    <img src={'favicon.png'} alt={'profile picture'}/>
+                </div>
             </div>
-            <h2>Strengths</h2>
-            <Card title={'Developer Advocate'}>
-                I led effort to upgrade our React pipeline, improving developer experience by 50%.
-            </Card>
-            <br/>
-            <Card title={'Obsessive Problem Solver'}>
-                I use Root Cause Analysis to debug, continuously isolating the problem until finding the root issue.
-            </Card>
-            <br/>
-            <Card title={'Continuous Learner'}>
-                I'm committed to continually improving my skills, implementing best practices and creating groundbreaking designs.
-            </Card>
-            <br/>
-            <h2>Skills</h2>
-            <Card>
-                React, JavaScript, TypeScript, Webpack 5, Node.js, REST, API Design, AWS (Lambda, API Gateway, EC2, S3, Cloudwatch, IAM), C# .NET, Python, Ruby on Rails, MongoDB, Microservices Architecture, Unit Tests, Integration Tests, Problem Solving, Continuous Learning, Agile, Scrum
-            </Card>
+            <div className={'px-10 py-40 bg-cyan-900'}>
+                <p className={'text-4xl [line-height:1.25] text-white'}>I'm an avid software engineer, obsessive problem
+                    solver, developer advocate. Boosted developer experience on my team by 50%.</p>
+            </div>
+            <div className={'m-5 flex flex-wrap space-x-5'}>
+                <div className={'px-5 py-10 w-[90%] md:w-[50%]'}>
+                    <h2>Specialties</h2>
+                    <Card title={'Developer Advocate'}>
+                        I led effort to upgrade our React pipeline, improving developer experience by 50%.
+                    </Card>
+                    <br/>
+                    <Card title={'Obsessive Problem Solver'}>
+                        I use Root Cause Analysis to debug, continuously isolating the problem until finding the root
+                        issue.
+                    </Card>
+                    <br/>
+                    <Card title={'Continuous Learner'}>
+                        I'm committed to continually improving my skills, implementing best practices and creating
+                        groundbreaking designs.
+                    </Card>
+                </div>
+                <div className={'px-5 py-10 w-[90%] md:w-[40%]'}>
+                    <h2>Skills</h2>
+                    <Card>
+                        React, JavaScript, TypeScript, Webpack 5, Node.js, REST, API Design, AWS (Lambda, API Gateway,
+                        EC2, S3, Cloudwatch, IAM), C# .NET, Python, Ruby on Rails, MongoDB, Microservices Architecture,
+                        Unit Tests, Integration Tests, Problem Solving, Continuous Learning, Agile, Scrum
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
