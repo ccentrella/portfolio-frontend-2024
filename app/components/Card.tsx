e0ad550af5375549d327d7e5d27258204a5b1f72
Christopher Centrella <ccentrella.j@gmail.com>
2024-02-24 12:08:18 -0600

    update card component

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -8,7 +8,7 @@ function Card({title, subtitle, image, children}: {
 }) {
     const titleColor = subtitle ? 'text-cyan-100' : 'text-cyan-300';
     return (
-        <div className={'p-7 max-w-96 rounded-2xl bg-primary shadow-xl shadow-cyan-100 hover:bg-cyan-950 hover:shadow-2xl hover:shadow-cyan-300'}>
+        <div className={'p-7 lg:p-12 rounded-2xl max-lg:w-[100%] lg:basis-2/5 bg-primary shadow-xl shadow-cyan-100 hover:bg-cyan-950 hover:shadow-2xl hover:shadow-cyan-300'}>
             <div className={'flex'}>
                 {image && <img src={image} alt={'card image'} className={'w-12 h-12 mr-5'}/>}
                 <div>
