a39a21e918274015e02c7e053b01648896cc27c7
Christopher Centrella <ccentrella.j@gmail.com>
2024-03-16 11:39:05 -0500

    add responsive navbar, update navbar icons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
 
 function Header() {
+
+    const iconList = [
+        {
+            id: 0,
+            src: './icons/light_mode_icon.svg',
+            alt: 'dark mode toggle',
+            href: null
+        },
+        {
+            id: 1,
+            src: './icons/adhd_icon.png',
+            alt: 'adhd mode toggle',
+            href: null
+        },
+        {
+            id: 2,
+            src: './icons/github_icon.svg',
+            alt: 'github link',
+            href: 'https://github.com/ccentrella'
+        }
+    ];
+    const icons = iconList.map(icon => {
+        const image = <img src={icon.src} alt={icon.alt} className={'w-7 rounded p-[0.32rem] bg-[#113D4D] hover:bg-cyan-800 transition-colors'}/>;
+
+        return icon.href ? (
+            <a key={icon.id} href={icon.href} rel={'noopener'} target={'_blank'}>
+                {image}
+            </a>
+        ) : image;
+    });
+
     return (
-        <div className={'px-7 py-4 bg-[#023142] flex justify-between items-center'}>
-            <img src={'./favicon.png'} alt={'profile picture logo'} className={'w-9'}/>
-            <div className={'flex gap-9 text-sm'}>
-                <a href="/"><p>Experience</p></a>
-                <a href="/interests"><p>Interests</p></a>
-                <a href="/blog"><p>Blog</p></a>
-                <a href="/contact"><p>Contact</p></a>
+        <div className={'px-7 py-4 bg-[#023142] flex flex-wrap justify-between items-center'}>
+            <div className={'flex items-center gap-3'}>
+                <a href="/"><img src={'./favicon.png'} alt={'profile picture logo'} className={'w-10'}/></a>
+                <p className={'text-cyan-100 md:hidden'}>Experience</p>
             </div>
-            <div className={'flex gap-4'}>
-                <img src={'./icons/light_mode_icon.svg'} alt={'dark mode toggle'} className={'w-5'}/>
-                <img src={'./icons/adhd_icon.png'} alt={'adhd mode toggle'} className={'w-5'}/>
-                <a href="https://github.com/ccentrella" rel={'noopener'} target={'_blank'}
-                   className={'hover:opacity- transition-opacity'}><img src={'./icons/github_icon.svg'}
-                                                                        alt={'github link'} className={'w-5'}/></a>
+            <div className={'flex gap-1.5 img md:order-last'}>
+                {icons}
+            </div>
+            <label htmlFor={'navbar-toggle'} className={'md:hidden'}>
+                <img src={'./icons/menu.svg'} alt={'menu button'} className={'w-8 cursor-pointer'}/>
+            </label>
+            <input id={'navbar-toggle'} className={'navbar-toggle'} type={'checkbox'}/>
+            <div className={'navbar'}>
+                <p><a href="/">Experience</a></p>
+                <p><a href="/interests">Interests</a></p>
+                <p><a href="/blog">Blog</a></p>
+                <p><a href="/contact">Contact</a></p>
             </div>
         </div>
     );
