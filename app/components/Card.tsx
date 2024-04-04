fd3b7ad051a34737801921abb6d295c161a3b050
Christopher Centrella <ccentrella.j@gmail.com>
2024-04-03 21:24:55 -0500

    changes to Card component

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card({tag, tagBackground='bg-cyan-700', socials=null, children}) {
     return (
-        <div className={'w-[100%] lg:w-96 lg:basis-5/12 p-7 bg-primaryDark rounded-2xl'}>
+        <div className={'w-[100%] lg:w-96 lg:basis-5/12 p-12 bg-primaryDark rounded-2xl'}>
             <span className={`px-5 py-2 rounded-[50px] text-sm ${tagBackground} text-cyan-100`}>{tag}</span>
             <div className={'my-7 text-cyan-10'}>
                 {children}
