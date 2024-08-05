1c3770d6845dd0309d8a7aaa250ecd538d91dfc2
Christopher Centrella <ccentrella.j@gmail.com>
2024-08-05 07:03:42 -0500

    add section titles to homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,20 +2,34 @@ import React from 'react';
 
 function Index(props) {
     return (
-        <div className={'min-h-[100vh] bg-[#037D97] grid grid-cols-[3.8fr_6.2fr]'}>
-            <div className={'flex flex-col justify-between p-14'}>
-                <div className={'flex flex-col justify-center'}>
-                    <img src={'profile.png'} alt={'profile picture'} className={'w-[50%] rounded-2xl'}/>
+        <>
+            <div className={'min-h-[100vh] bg-[#037D97] grid grid-cols-[3.8fr_6.2fr]'}>
+                <div className={'flex flex-col justify-between p-14'}>
+                    <div className={'flex flex-col justify-center'}>
+                        <img src={'profile.png'} alt={'profile picture'} className={'w-[50%] rounded-2xl'}/>
+                    </div>
+                    <div className={''}>
+                        <p className={'text-cyan-200'}>&lsquo;Sup? My name is </p>
+                        <p className={'text-4xl font-medium'}>Chris Centrella.</p>
+                    </div>
                 </div>
-                <div className={''}>
-                    <p className={'text-cyan-200'}>Sup? My name is </p>
-                    <p className={'text-4xl font-medium'}>Chris Centrella.</p>
+                <div className={'flex flex-col justify-center p-24'}>
+                    <div className={'min-h-[65vh] bg-cyan-300 rounded-2xl'}></div>
                 </div>
             </div>
-            <div className={'flex flex-col justify-center p-24'}>
-                <div className={'min-h-[65vh] bg-cyan-300 rounded-2xl'}></div>
+            <div>
+                <h2 className={'text-4xl px-12 py-24'}>Industry Experience.</h2>
+                <div></div>
             </div>
-        </div>
+            <div>
+                <h2 className={'text-4xl px-12 py-24'}>Technical Experience.</h2>
+                <div></div>
+            </div>
+            <div>
+                <h2 className={'text-4xl px-12 py-24'}>Causes.</h2>
+                <div></div>
+            </div>
+        </>
     );
 }
 
